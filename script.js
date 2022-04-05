10c7a9a2bda270bcd7aed562aa3e5490a8dfc6d2
Brandon Moye <85130587+Brandon-Moye@users.noreply.github.com>
2022-04-04 19:37:52 -0500

    Update script.js
    changing to portfolio instead of tic-tac-toe

--- script.js
+++ script.js
@@ -1,256 +1,106 @@
 "use strict";
 
-let clickCount = 0; //used to check for ties later
-let piece = ""; //will either be an X or O
-/**
- * try adding a this key word to help get the function to be refracted
- */
-document.querySelector(".r1c1").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-    document.querySelector(".r1c1").textContent = piece;
+// document.getElementById("imageContainer").addEventListener("mouseover", function() {
+//     console.log("this is clicked");
+// })
 
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
+// $(document).ready(function () {
+//   if (window.location.hash) {
+//     //bind to scroll function
+//     $(document).scroll(function () {
+//       var hash = window.location.hash;
+//       var hashName = hash.substring(1, hash.length);
+//       var element;
 
-document.querySelector(".r1c2").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
+//       //if element has this id then scroll to it
+//       if ($(hash).length != 0) {
+//         element = $(hash);
+//       }
+//       //catch cases of links that use anchor name
+//       else if ($('a[name="' + hashName + '"]').length != 0) {
+//         //just use the first one in case there are multiples
+//         element = $('a[name="' + hashName + '"]:first');
+//       }
 
-    document.querySelector(".r1c2").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r1c3").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r1c3").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r2c1").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r2c1").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r2c2").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r2c2").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r2c3").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r2c3").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r3c1").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r3c1").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r3c2").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r3c2").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-document.querySelector(".r3c3").addEventListener(
-  "click",
-  function a1() {
-    if (piece === "O") {
-      piece = "X";
-    } else {
-      piece = "O";
-    }
-    clickCount++;
-
-    document.querySelector(".r3c3").textContent = piece;
-
-    variables();
-  },
-  { once: true } //experimenting now put this back in play once I have this figured out
-);
-
-//experimenting with trying to shorten the code
-// function a1(cell) {
-//   if (piece === "O") {
-//     piece = "X";
-//   } else {
-//     piece = "O";
+//       //if we have a target then go to it
+//       if (element != undefined) {
+//         window.scrollTo(0, element.position().top);
+//       }
+//       //unbind the scroll event
+//       $(document).unbind("scroll");
+//     });
 //   }
-//   clickCount++;
-//   document.querySelector(cell).textContent = piece;
+// });
 
-//   variables();
-// }
+// --------------------------
+// SMOOTH SCROLLINGH
+// --------------------------
+const allLinks = document.querySelectorAll("a:link");
 
-//this function checks the correct patterns
-function variables() {
-  //made variable names to make the if statement more legible
-  let a1 = document.querySelector(".r1c1").textContent;
-  let a2 = document.querySelector(".r1c2").textContent;
-  let a3 = document.querySelector(".r1c3").textContent;
-  let a4 = document.querySelector(".r2c1").textContent;
-  let a5 = document.querySelector(".r2c2").textContent;
-  let a6 = document.querySelector(".r2c3").textContent;
-  let a7 = document.querySelector(".r3c1").textContent;
-  let a8 = document.querySelector(".r3c2").textContent;
-  let a9 = document.querySelector(".r3c3").textContent;
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-  //using if statements to see if cells = i.e. a winning pattern
-  if (
-    (a1 === a2 && a1 === a3) ||
-    (a1 === a4 && a1 === a7) ||
-    (a1 === a5 && a1 === a9) ||
-    (a2 === a5 && a2 === a8) ||
-    (a3 === a6 && a3 === a9) ||
-    (a3 === a5 && a3 === a7) ||
-    (a4 === a5 && a4 === a6) ||
-    (a7 === a8 && a7 === a9)
-  ) {
-    // console.log(`${piece} You're the winner!`);
-    //
-    //need to focus on adding an element that ends the game and doesn't allow the player to continue to click
-    //turning the console.log into a query selector
-
-    //if the statement is met then whoever won will be displayed using a template literal
-    document.getElementById("finalMessage").classList.remove("hidden");
-
-    //in order to not allow players to continue the game we will remove the tic-tac-toe grid and a message
-    document.getElementById("container").classList.add("hidden");
-    document.querySelector(
-      ".addMessage"
-    ).textContent = `${piece} You're the winner!`; //announcing the winner will appear!
-
-    /*adds the reset button once someone has won, using a hidden class to keep it
-    not visible during the game and removes the hidden class once someone has won
-    */
-    document.getElementById("reset").classList.remove("hidden");
-  } else {
-    console.log("Keep going!"); //just something to help me when writing the code
-  }
-
-  //the reset button not necessarily letting us repeat functions with the once true property
-  //decided to reload the page instead so this prevents plays from clicking on a cell more than one and change the piece
-  document.querySelector(".reset").addEventListener("click", function () {
-    window.location.reload();
+    //Scroll back to top
+    if (href === "#aboutSection")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#aboutSection" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
   });
+});
 
-  /*if a there is a win, the code above will fire
-  this is an if statement in the events of a tie
-  */
-  if (clickCount === 9) {
-    // console.log(`${piece} You're the winner!`); //just something in the beginning phases of code
-    //turning the console.log into a query selector
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
-    //
+// ------------------------------------------------------
 
-    /*
-    the final message also have a hidden class that is removed once the click
-    and add a hidden class to the container elements to removed the grid
-    */
-    document.getElementById("finalMessage").classList.remove("hidden");
-    document.getElementById("container").classList.add("hidden");
-    document.querySelector(
-      ".addMessage"
-    ).textContent = `A TIE! Please Try Again!`;
-    //in order to not allow players to continue the game we will remove the tic-tac-toe grid and a message
-    //announcing the winner will appear!
+// document
+//   .querySelector(".imageContainer")
+//   .addEventListener("mouseover", function () {
+//     document.getElementById("githubLogo").classList.add("githubLogo2");
+//   });
 
-    //adds the reset button once someone has won
-    document.getElementById("reset").classList.remove("hidden");
-  } else {
-    // console.log("Keep going!"); //something for when I was developing
+// document
+//   .querySelector(".projectDescription")
+//   .addEventListener("mouseover", function () {
+//     document.getElementById("githubLogo").classList.remove("githubLogo2");
+//   });
+
+//adding the sticky class when it scrolls into the right view
+
+//intersection observer
+const sectionHeroEl = document.querySelector(".heroContainer");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+      document;
+      // .querySelector(".allNavContainer")
+      // .classList.remove("allNavContainerWithAbsolutePositioning");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+      document;
+      // .querySelector(".allNavContainer")
+      // .classList.add("allNavContainerWithAbsolutePositioning");
+    }
+  },
+  {
+    // inside the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-90px", //fire when the heroContainer is not in the viewport
   }
+);
 
-  //the reset button not necessarily letting us repeat functions with the once true property
-  document.querySelector(".reset").addEventListener("click", function () {
-    window.location.reload();
-  });
-}
+obs.observe(sectionHeroEl);
